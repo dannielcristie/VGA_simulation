e7e26a799772104b542a5db5f30bceca53f55c19
Danniel_Cristie <dany14012@gmail.com>
2019-12-02 00:42:39 -0300

    plot_oct

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from mpl_toolkits import mplot3d
-import numpy as np 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
+def f(x,y):
+    return print(2*x**2+y**2)
 
+u = np.linspace(-2,2,10); v = np.linspace(-2,2,10)
 
+u, v = np.meshgrid(u, v)
+
+x = u; y = v; z = f(x,y)
+
+fig = plt.figure()
+
+ax = plt.axes(projection='3d')
+
+ax.contour3D(x, y, 30)
+plt.show()
